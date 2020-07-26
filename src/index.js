d1cf23a6e70ae0deacad5fd75fe6ed294aecd697
Nicholas <tao.nicholas1@gmail.com>
2020-07-26 17:12:04 -0400

    finish tutorial

--- src/index.js
+++ src/index.js
@@ -4,46 +4,18 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
 
-//STORE --> globalized state
-
-//ACTION INCREMENT --> a function that returns an object
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-let store = createStore(counter);
-
-//display in console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
+const myStore = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={myStore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
